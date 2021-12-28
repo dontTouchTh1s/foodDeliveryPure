1c6e28a90485cac3b00eb503262f130e14c6a752
dontTouchTh1s <alim201381@gmail.com>
2021-12-28 13:22:04 +0330

    Change display to grid Add product basket button

--- public/header.php
+++ public/header.php
@@ -22,19 +22,31 @@ if (isset($_SESSION['id']) and isset($_SESSION['email'])) {
             <li>
                 <a href="/"> بلاگ </a>
             </li>
+
         </ul>
     </nav>
-    <a id="user-name" href="<?= PUBLIC_URL . '/users/edit-profile.php' ?>" class="user-register"><?= $name ?></a>
-    <a id="login" href="<?= PUBLIC_URL . '/users/login.php' ?>" class="user-register">ورود به حساب</a>
-
-    <i class="user-logo" id="user-logo"></i>
-    <div class="user-manage" aria-hidden="true" id="user-manage">
-        <div class="flex-100">
-            <p class="flex-100" id="user-name"><?= $name ?></p>
-            <p class="flex-100" id="user-email"><?= $email ?></p>
-        </div>
-        <div class="flex-100">
-            <a href="<?= ACTIONS_URL . '/users/exit-action.php' ?>" class="btn btn-outlined flex-100">خروج</a>
+    <div class="user-navigation">
+        <?php
+        if ($logeIn == "true") {
+            echo("
+        <a id='product-basket' href='' class='btn btn-outlined'>        
+        <span>سبد خرید</span>
+        <i class='fas fa-shopping-basket'></i>
+        </a>
+        ");
+        }
+        ?>
+        <a id="user-name" href="<?= PUBLIC_URL . '/users/edit-profile.php' ?>" class="user-register"><?= $name ?></a>
+        <a id="login" href="<?= PUBLIC_URL . '/users/login.php' ?>" class="user-register">ورود به حساب</a>
+        <i class="user-logo" id="user-logo"></i>
+        <div class="user-manage" aria-hidden="true" id="user-manage">
+            <div class="flex-100">
+                <p class="flex-100" id="user-name"><?= $name ?></p>
+                <p class="flex-100" id="user-email"><?= $email ?></p>
+            </div>
+            <div class="flex-100">
+                <a href="<?= ACTIONS_URL . '/users/exit-action.php' ?>" class="btn btn-outlined flex-100">خروج</a>
+            </div>
         </div>
     </div>
 </header>
