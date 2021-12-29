ea174af1d0c5134f7dacdeed7e525bfd96279e9c
dontTouchTh1s <alim201381@gmail.com>
2021-12-29 13:26:10 +0330

    ...

--- php-actions/classes/Mysql.php
+++ php-actions/classes/Mysql.php
@@ -2,8 +2,28 @@
 
 class Mysql
 {
-    public function Query($query)
+    public mysqli $db;
+    public string $connectError;
+
+    public function __construct()
     {
 
+    }
+
+    public function connect($host, $username, $password, $dataBase): bool
+    {
+        $mysql = new mysqli($host, $username, $password, $dataBase);
+        $this->db = $mysql;
+        if ($mysql->connect_errno) {
+            $this->connectError = $mysql->connect_error;
+            return false;
+        } else
+            return true;
+    }
+
+    public function query($query)
+    {
+        $sth = $this->db->prepare($query);
+        $sth->bind_param('ii', $uid, $pid);
     }
 }
