11f9be59ac3bd2499ed3a9dcf82de3f60c9969b1
dontTouchTh1s <alim201381@gmail.com>
2021-12-29 13:05:30 +0330

    Add file. Show products in user's product basket

---
+++ public/users/product-basket.php
@@ -0,0 +1,52 @@
+<?php
+include("__PATH__.php");
+Authentication::check_login();
+$error = "";
+$productBasketList = [];
+include(USERS_PATH . "/products/product-basket-action.php");
+?>
+<!DOCTYPE html>
+<html lang="fa">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="contact us">
+    <meta name="author" content="AliM">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="<?= STYLE_URL . '/style.css' ?>">
+    <script src="https://kit.fontawesome.com/f5a43cdea2.js" crossorigin="anonymous"></script>
+    <title>ورود</title>
+</head>
+<body class="layout">
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<?php
+include(PUBLIC_PATH . '/header.php');
+include(ASSETS_PATH . "/template/customer-navigation-drawer.php");
+?>
+<aside>
+    <div class="content content-aside">
+    </div>
+</aside>
+<div class="container">
+    <div class="content content-products">
+        <div class="card-container">
+            <?php
+            foreach ($productBasketList as $product) {
+                $card = new card(CARD_FILLED, $product[0]);
+                $card->title($product[3]);
+                $card->subhead("subhead");
+                $card->description($product[5]);
+                $card->picture($product[6]);
+                $card->button("سفارش دهید", CARD_BTN_OUTLINED);
+                $card->action(CARD_ACTION_LIKE);
+                $card->action(CARD_ACTION_BOOKMARK);
+                $card->add();
+            }
+            ?>
+        </div>
+    </div>
+</div>
+<?php include(PUBLIC_PATH . '/footer.php'); ?>
+<script src="<?= JS_URL . '/javaScriptDynamicLoad.js' ?>" type="text/javascript"></script>
+</body>
+</html>
