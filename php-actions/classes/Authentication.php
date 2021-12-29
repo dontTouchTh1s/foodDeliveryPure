d46f6224612f973a11c8569fd31a45ebb218ed23
dontTouchTh1s <alim201381@gmail.com>
2021-12-29 12:58:02 +0330

    Close session after using methods

--- php-actions/classes/Authentication.php
+++ php-actions/classes/Authentication.php
@@ -5,22 +5,26 @@ class Authentication
     static function isLogeIn(): bool
     {
         session_start();
-        if (isset($_SESSION['id'])) {
-            return true;
-        } else
-            return false;
+        $session = isset($_SESSION['id']);
+        session_write_close();
+        return $session;
     }
 
     static function check_login()
     {
+        session_start();
         if (!isset($_SESSION['id'])) {
+            session_write_close();
             redirect::request("/front-project/public/index.php");
         }
+        session_write_close();
     }
 
     static function get_id(): bool
     {
         session_start();
-        return $_SESSION['id'] ?? false;
+        $session = $_SESSION['id'] ?? false;
+        session_write_close();
+        return $session;
     }
 }
