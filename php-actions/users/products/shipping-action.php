4cf0b05a15145f0e12040f10b208e296876bb1fd
dontTouchTh1s <alim201381@gmail.com>
2022-01-02 23:26:06 +0330

    shipping page, show address and time

---
+++ php-actions/users/products/shipping-action.php
@@ -0,0 +1,46 @@
+<?php
+
+include(INCLUDES_PATH . "/setting.php");
+
+// Check if user is not loged in
+$uid = Authentication::get_id();
+if (!$uid)
+    return;
+
+// Connecting to database
+$mysql = new mysqli(HOST, USERNAME, PASSWORD, DB);
+if ($mysql->connect_errno) {
+    $error = "عدم اتصال";
+    $errorDes = "در هنگام دریافت اطلاعات مشکلی پیش آمده است، لطفا بعدا تلاش کنید.";
+    $mbList[] = new message_box(MESSAGEBOX_TYPE_ERROR, $error);
+    return;
+}
+$query = "SELECT * FROM productsbasket WHERE user_id = ?";
+$sth = $mysql->prepare($query);
+$sth->bind_param('i', $uid);
+if ($sth->execute()) {
+    $result = $sth->get_result();
+    if ($result->num_rows > 0) {
+        $productBasketList = $result->fetch_all();
+        $totalPrice = 0;
+        foreach ($productBasketList as $product) {
+            $query = "SELECT * FROM products WHERE id = ?";
+            $sth = $mysql->prepare($query);
+            $sth->bind_param('i', $product[1]);
+            $sth->execute();
+            $result = $sth->get_result();
+            $productBasketListInfo[] = $result->fetch_assoc();
+            $totalPrice += $product[2];
+        }
+    } else {
+        $emptyBasket = true;
+        return;
+    }
+} else {
+    $error = "عدم اتصال";
+    $errorDes = "در هنگام دریافت اطلاعات مشکلی پیش آمده است، لطفا بعدا تلاش کنید.";
+    $mbList[] = new message_box(MESSAGEBOX_TYPE_ERROR, $error);
+    end($mbList)->description($errorDes);
+    return;
+}
+
---
+++ public/users/shipping.php
@@ -0,0 +1,62 @@
+<?php
+include("__PATH__.php");
+Authentication::check_login();
+$productBasketListInfo = $productBasketList = $mbList = [];
+/** @var int $totalPrice */
+include(USERS_PATH . "/products/shipping-action.php");
+?>
+<!DOCTYPE html>
+<html lang="fa">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="contact us">
+    <meta name="author" content="AliM">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="<?= STYLE_URL . '/style.css' ?>">
+    <script src="https://kit.fontawesome.com/f5a43cdea2.js" crossorigin="anonymous"></script>
+    <title>ورود</title>
+</head>
+<body class="layout">
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<?php
+include(PUBLIC_PATH . '/header.php');
+include(ASSETS_PATH . "/template/customer-navigation-drawer.php");
+?>
+<div class="container container-buying">
+    <div class="content content-shipping">
+        <div class="address-content">
+            <p>آدرس تحویل سفارش</p>
+            <p class="address"></p>
+            <p>گیرنده:
+                <span class="receiver-name"></span>
+            </p>
+        </div>
+    </div>
+    <div class="content content-aside">
+        <div class="checkout-order">
+            <ul class="prices">
+                <li>
+                    <span>جمع کالا ها:</span>
+                    <span><?= $totalPrice ?></span>
+                </li>
+                <li>
+                    <span>جمع سبد خرید:</span>
+                    <span><?= $totalPrice ?></span>
+                </li>
+            </ul>
+            <p>هزینه ارسال در ادامه فرایند خرید، بر اساس نحوه ارسال و زمان ارسال محاسبه خواهد شد.</p>
+            <button type="button" class="btn btn-filled flex-100 goto-pay">
+                ادامه فرایند خرید
+            </button>
+        </div>
+    </div>
+    <?php
+    foreach ($mbList as $ms)
+        $ms->add();
+    ?>
+</div>
+<?php include(PUBLIC_PATH . '/footer.php'); ?>
+<script src="<?= JS_URL . '/javaScriptDynamicLoad.js' ?>" type="text/javascript"></script>
+</body>
+</html>
