d5eb1d5f0c5043b91c65b6a166fca7dfc8110021
Ali Mashayekhi <alim201381@gmail.com>
2023-05-24 20:23:42 +0330

    fix setting db connect error handler

--- php-actions/includes/setting.php
+++ php-actions/includes/setting.php
@@ -4,12 +4,22 @@ const USERNAME = "root";
 const PASSWORD = "";
 const DB = "front";
 
+try {
+    /** @noinspection PhpObjectFieldsAreOnlyWrittenInspection */
+    $driver = new mysqli_driver();
+    $driver->report_mode = MYSQLI_REPORT_STRICT | MYSQLI_REPORT_ERROR;
+    $mysql = new mysqli();
+    $mysql->connect(HOST, USERNAME, PASSWORD);
+    $mysql->select_db(DB);
 
-$mysql = new mysqli(HOST, USERNAME, PASSWORD, DB);
-if ($mysql->connect_errno) {
-    $error = "در هنگام ثبت اطلاعات مشکلی پیش آمده لطفا بعدا دوباره تلاش کنید.";
-    return;
+} catch (mysqli_sql_exception $e) {
+    die(
+    "<h1>Error while connecting to database</h1>
+            <p/> Check database name is entered currently, and you created it before"
+    );
 }
+
+
 $query = "
 CREATE TABLE IF NOT EXISTS message (
     id INT AUTO_INCREMENT,
