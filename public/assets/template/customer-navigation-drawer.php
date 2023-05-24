6a7c2d095c3becc65260d1316bf4a35b01272859
Ali Mashayekhi <alim201381@gmail.com>
2023-05-24 18:40:25 +0330

    Add products section to navdrawer

--- public/assets/template/customer-navigation-drawer.php
+++ public/assets/template/customer-navigation-drawer.php
@@ -5,9 +5,14 @@ use Navigation\Navigation_drawer;
 /** @var string $onFocus */
 $nav = new Navigation_drawer("منو");
 $nav->setFocused($onFocus);
-$sec = $nav->section("new section");
+$sec = $nav->section("حساب کاربری");
 $sec->button("ورود", USER_URL . "/login.php", "fas fa-user-alt");
 $sec->button("ایجاد حساب", USER_URL . "/register.php", "fas fa-sign-in-alt");
+$nav->add_section($sec);
+
+$sec = $nav->section("محصولات");
+$sec->button("محصولات موردعلاقه", PUBLIC_URL . "/register.php", "fas fa-heart");
+$sec->button("محصولات ذخیره شده", PUBLIC_URL . "/register.php", "fas fa-bookmark");
 $nav->add_section($sec);
 $nav->web_page_section();
 $nav->add();
