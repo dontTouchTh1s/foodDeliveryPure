8fe0e41f6bfccdbd6451802c9875107bb964485b
Ali Mashayekhi <alim201381@gmail.com>
2023-05-24 20:24:01 +0330

    now seed a user too

--- php-actions/includes/seeder.php
+++ php-actions/includes/seeder.php
@@ -7,8 +7,10 @@ if ($mysql->connect_errno) {
     return;
 }
 $query = "INSERT INTO products (type, name, state, price, description, pictures)
-VALUES (1, 'test pizza', 1, 150000, 'test product', 'pizza.jpg')";
+VALUES (1, 'test pizza', 1, 150000, 'test product', 'pizza.jpg');
+INSERT INTO users (name, full_name, email, password, gender)
+VALUES ('test', 'lastname', 'test@gmail.com', 'password123', 1);";
 
 
-if ($mysql->query($query))
-    return "SEEDER RUN SUCCESSFULLY";
+if ($mysql->multi_query($query))
+    die("SEEDER RUN SUCCESSFULLY");
